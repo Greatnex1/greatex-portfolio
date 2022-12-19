5d0cc27c9d9a71830556f5626a37c6469aa5c705
Nouah <noahgreatakoni@gmail.com>
2022-12-20 00:48:56 +0100

    greatnex

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,7 +12,7 @@ const Portfolio = () => {
             id:1,
             src: ecommerce,
             title:"an online shopping program",
-            link: ""
+            href: "https://github.com/"
         },
 
         {
@@ -47,21 +47,19 @@ const Portfolio = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
 
 
-        {portfolios.map(({id, src, title, link}) => (
+        {portfolios.map(({id, src, title, href}) => (
 
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
                 <img  
                 src={src}
-                
                 alt=""
-               
                 className="rounded-md duration-200 hover:scale-105"
                 />
                
             <div className="flex items-center justify-center">
             {title}
             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
-            >
+            ><a href={href} target="_blank" >click here </a>
             </button>
 
             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
