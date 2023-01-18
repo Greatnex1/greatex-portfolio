ed0a623d14dde1800496fc11a9fa51c463c69644
Nouah <noahgreatakoni@gmail.com>
2023-01-18 20:57:59 +0100

    greatnex

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -40,7 +40,7 @@ const NavBar = () => {
     <div className='flex justify-between items-center w-full h-20
     px-4 text-white bg-black fixed'>
         <div>
-            <h1 className='text-4xl font-signature ml-1' >Nouah </h1>
+            <h1 className='text-4xl font-signature ml-1' >Nouah Great</h1>
         </div>
 
         <ul className='hidden md:flex'>
